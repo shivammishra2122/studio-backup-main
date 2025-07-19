109eccbeb5c603abcc45be04ed0cf3729d5676c1
shivam mishra <mishrashivam7233992@gmail.com>
2025-07-19 11:01:36 +0530

    build changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
+import withBundleAnalyzer from '@next/bundle-analyzer';
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: true,
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -26,4 +31,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
--- src/app/patients/[id]/blood-center/page.tsx
+++
@@ -1,5 +0,0 @@
-return (
-    <div className="flex flex-col h-[calc(100vh-var(--top-nav-height,40px))] bg-background text-sm">
-        {/* Your Blood Center Content Here */}
-    </div>
-); 
